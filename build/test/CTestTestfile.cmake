5f6d3db0b3a920dc4e63533dae9107831fdc1fbc
Roger <13896063439@163.com>
2023-05-15 21:57:36 +0800

    first test pass

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/roger/infer/KuiperKuiper/test
+# Build directory: /home/roger/infer/KuiperKuiper/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <glog/logging.h>
+
+int main() {
+  LOG(INFO) << "Kuiper Project Init";
+  return 0;
+}
